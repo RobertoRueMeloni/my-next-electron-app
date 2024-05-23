49458d69287e2ca107c52784cc7a715af3511db7
Roberto Meloni <94394668+RobertoRueMeloni@users.noreply.github.com>
2024-05-23 16:32:48 -0700

    test

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional, for using Electron APIs in renderer process
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL('http://localhost:3000'); // Load your Next.js app
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
